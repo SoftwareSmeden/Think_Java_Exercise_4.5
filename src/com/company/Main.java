692f5ea00e0112b2013e6dcd36bd366a78480ef9
SoftwareSmeden <torb815g@edu.easj.dk>
2021-09-09 10:05:10 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//Think Java - Exercise 4.5
+
+        int bizz = 5;
+        int buzz = 2;
+        zoop("just for", bizz);
+        clink(2 * buzz);
+    }
+
+    public static void zoop(String fred, int bob) {
+            System.out.println(fred);
+        if (bob == 5) {
+                ping("not ");
+        }
+        else
+            System.out.println("!");{
+        }
+    }
+    public static void clink(int fork) {
+        System.out.print("It's ");
+        zoop("breakfast ", fork);
+    }
+    public static void ping(String strangStrung) {
+        System.out.println("any " + strangStrung + "more ");
+    }
+}
